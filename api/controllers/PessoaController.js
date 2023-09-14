114a4c0d987c5ed3ec827a5b3a711cab0fd86100
FelipeGarLima <meca_felipe@hotmail.com>
2023-09-14 14:15:19 -0300

    feat: criando métodos de crud

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,6 +1,7 @@
 const database = require('../models')
 
 class PessoaController {
+
     static async pegaTodasAsPessoas(req, res){
         try {
             const todasAsPessoas = await database.Pessoas.findAll()
@@ -9,6 +10,28 @@ class PessoaController {
             return res.status(500).json(error.message)
         }
     }
+
+    static async pegaUmaPessoa(req, res){
+        try {
+            const { id } = req.params
+            //const pessoa = await database.Pessoas.findAll({ where: { id: Number(id)}}) // retorna um vetor com um objeto
+            const pessoa = await database.Pessoas.findOne({ where: { id: Number(id)}}) // retorna um objeto
+            return res.status(200).json(pessoa)
+        } catch (error) {
+            return res.status(500).send({message: error.message}) // completar tratamento
+        }
+    }
+
+    static async criaPessoa(req, res){
+        try {
+            await database.Pessoas.create(req.body)
+            res.status(200).send({message: "Cadrastado com sucesso!"})
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async 
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.post('/pessoas', PessoaController.criaPessoa)
 
 module.exports = router
